fccf5d08f5d1aab48a2c6c2e3be63eeea2256bd8
khoald-1241 <lam.dang.khoa@sun-asterisk.com>
2022-03-09 16:16:55 +0700

    Chap 8: Log in và Log out | Đã fix lần 1

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
     get "/signup", to: "users#new"
     resources :users
+
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
